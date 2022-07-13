c0cbada8f1e199de4c633ded6b1395c5472783a6
racharlaharsha <108944278+racharlaharsha@users.noreply.github.com>
2022-07-13 08:45:59 +0530

    updating comments for each line

--- face_detection.py
+++ face_detection.py
@@ -1,20 +1,27 @@
+#importing opencv2 module
 import cv2
 
-classifier =  cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-pic = cv2.imread('test1.jpg')
-graypic= cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
+#using pretrained models one by one, the cascade classifier takes file name as argument
+classifier =  cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt.xml')
+
+#reading a sample image
+pic = cv2.imread('test9.jpg')
+
+#resizing all images for consistent results and easy analysis
+pic = cv2.resize(pic,(450,350))
+
+#using the detectMultiScale function returns bounding boxes of detected faces
 bboxes = classifier.detectMultiScale(pic)
-for box in bboxes:
-    print(box)
+
+#for each box a rectangle is been drawn to identify clearly
 for box in bboxes:
     x,y,width,height = box
     x2,y2=x+width,y+height
     cv2.rectangle(pic,(x,y),(x2,y2),(255,255,0))
-    
-for box in bboxes:
-    print(box)
-    
-cv2.imshow('facedetection',pic)
-cv2.waitKey()
 
-    
+
+#To display the output picture with detected faces
+cv2.imshow('facedetection',pic)
+
+#wait key is used to tell how much time the output must be displayed
+cv2.waitKey()
